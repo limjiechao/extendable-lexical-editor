77132a4240fcf7341b8ca959f2fa319d6c8f8fed
Lim Jiechao <jiechao.lim@gmail.com>
2025-11-20 14:30:11 +0800

    fix: new logic for detecting playground

--- src/appSettings.ts
+++ src/appSettings.ts
@@ -7,9 +7,7 @@
  */
 
 const hostName = window.location.hostname;
-export const isDevPlayground: boolean =
-  hostName !== 'playground.lexical.dev' &&
-  hostName !== 'lexical-playground.vercel.app';
+export const isDevPlayground: boolean = hostName.endsWith('vercel.app');
 
 export const DEFAULT_SETTINGS = {
   disableBeforeInput: false,
